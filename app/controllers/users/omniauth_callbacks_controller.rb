63b20e3c6eccec45e7cce746d0b65da251903fca
korabieka <hussienashour@ymail.com>
2016-05-05 01:33:28 +0200

    facebook login modifing

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,7 +8,7 @@ def facebook
       set_flash_message(:notice, :success, :kind => "Facebook") if is_navigational_format?
     else
       session["devise.facebook_data"] = request.env["omniauth.auth"]
-      redirect_to edit_user_registration_url
+     # redirect_to edit_user_registration_url
     end
   end
 
