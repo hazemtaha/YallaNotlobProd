cd48d21fcc860a0dca4da9824234ce51a9561cd4
Mostafa Abd El Fattah <mostafa.abdelfattah3@gmail.com>
2016-05-05 14:10:35 +0200

    Finished all operations with groups and friends

--- app/assets/javascripts/groups/ajax_remote_create.js
+++ app/assets/javascripts/groups/ajax_remote_create.js
@@ -2,7 +2,7 @@ $(function(){
 	$('#formGroup').on('ajax:success',function(e, data, status, xhr){
 		//if success
 		console.log(data.name);
-		$('#table').append("<tr><td>"+data.name+"</td><td><a id='group"+data.id+"' href='#' class='addGroup'>Add</a></td><td><a href='#'>Destroy</a></td></tr>");
+		$('#table').append("<tr><td>"+data.name+"</td><td><a id='group"+data.id+"' href='#' class='addGroup'>Add</a></td><td><a data-confirm='Are you sure you want to delete group?' rel='nofollow' data-method='delete' href='/groups/"+data.id+"'>Destroy</a></td></tr>");
 
 	}).on('ajax:error',function(e, data, status, xhr){
 		// alert("Haloooooooooooooooooo");
