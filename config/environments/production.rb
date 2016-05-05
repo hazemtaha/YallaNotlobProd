d303cf683e36f9d8a049206d805e025831a83d84
korabieka <hussienashour@ymail.com>
2016-05-05 05:06:16 +0200

    editing forget password

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,5 +77,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   #reset password
-  config.action_mailer.default_url_options = { :host => 'http://yallanotlob-hazemt.rhcloud.com' }
-  end
+  config.action_mailer.default_url_options = { :host => 'http://yallanotlob-hazemt.rhcloud.com' } 
+ end
