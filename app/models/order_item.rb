d6cfe85a7546e8fdea5e6e3c25e3f4b788be7248
Hazem Taha <thatskan@gmail.com>
2016-05-06 23:14:34 +0200

    order details validations

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -3,7 +3,7 @@ class OrderItem < ActiveRecord::Base
   belongs_to :order
   default_scope { order('created_at DESC') }
   validates :item, presence: true
-  validates :amount, presence: true
-  validates :price, presence: true
-
+  validates :amount, presence: true, numericality: { greater_than: 0 }
+  validates :price, presence: true, numericality: { greater_than: 0 }
+  
 end
